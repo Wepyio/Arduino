e3bb864c986dc41a5b8a728b2238d2d274c21819
Wepyio <wepyio@yahoo.es>
2019-05-23 07:44:21 -0600

    Create brazo.ino

---
+++ brazo.ino
@@ -0,0 +1,26 @@
+#include <Servo.h>
+
+int miservo1 = 9;
+int miservo2 = 10;
+int miservo3 = 13;
+int miservo4 = 14;
+
+Servo miservo1;
+Servo miservo2;
+Servo miservo3;
+Servo miservo4;
+
+int base = 23;
+int codo = 24;
+int antebrazo = 25;
+
+int leerbase = 0;
+int leercodo = 0;
+int leerantebrazo = 0;
+
+void setup(){
+	
+}
+
+void loop(){
+}
